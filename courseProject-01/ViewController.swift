76064f00be42a3cfe99bca85bec7247f74fb6998
Ri Do <70012061+ridohendrawan@users.noreply.github.com>
2022-05-19 12:56:00 +0700

    Label, UIImage, & Appicon

---
+++ courseProject-01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-AngelaYu-Boothcamp_Practice
+//
+//  Created by Rido Hendrawan on 18/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
